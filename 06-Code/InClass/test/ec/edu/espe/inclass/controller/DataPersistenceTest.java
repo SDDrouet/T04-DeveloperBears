196b0f636e3efaaa610db7b94ae2ba99de4bab50
sddrouet@espe.edu.ec <sddrouet@espe.edu.ec>
2023-02-28 23:29:29 -0500

    V2.3.1 Unit Cases

--- 06-Code/InClass/test/ec/edu/espe/inclass/controller/DataPersistenceTest.java
+++ 06-Code/InClass/test/ec/edu/espe/inclass/controller/DataPersistenceTest.java
@@ -1,10 +1,10 @@
 package ec.edu.espe.inclass.controller;
 
 import ec.edu.espe.inclass.model.Course;
-import ec.edu.espe.inclass.model.Teacher;
+import ec.edu.espe.inclass.model.Student;
+import java.util.ArrayList;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import utils.DBManager;
 
 /**
  *
@@ -26,6 +26,19 @@ public class DataPersistenceTest {
         boolean result = instance.connectMongoDB();
         assertEquals(expResult, result);
     }
+    
+    /**
+     * Test of updateData method, of class DataPersistence.
+     */
+    @Test
+    public void testUpdateData() {
+        System.out.println("updateData");
+        DataPersistence instance = DataPersistence.getInstance();
+        instance.connectMongoDB();
+        boolean expResult = true;
+        boolean result = instance.updateData();
+        assertEquals(expResult, result);
+    }
 
     /**
      * Test of updateData method, of class DataPersistence.
@@ -34,7 +47,7 @@ public class DataPersistenceTest {
     public void testUpdateDataOffLine() {
         System.out.println("updateData");
         DataPersistence instance = DataPersistence.getInstance();
-        boolean expResult = true;
+        boolean expResult = false;
         boolean result = instance.updateData();
         assertEquals(expResult, result);
     }
@@ -49,6 +62,105 @@ public class DataPersistenceTest {
         DataPersistence instance = DataPersistence.getInstance();    
         instance.connectMongoDB();
         boolean expResult = true;
+        boolean result = instance.updateStudentsInDB(course);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of updateStudentsInDB method, of class DataPersistence.
+     */
+    @Test
+    public void testUpdateStudentsCourseEmpty() {
+        System.out.println("updateStudentsInDB");      
+        Course course = new Course("prueba", 100);
+        DataPersistence instance = DataPersistence.getInstance();    
+        instance.connectMongoDB();
+        boolean expResult = true;
+        boolean result = instance.updateStudentsInDB(course);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of updateStudentsInDB method, of class DataPersistence.
+     */
+    @Test
+    public void testUpdateStudentsCourseOneStudentWrong() {
+        System.out.println("updateStudentsInDB");
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("1236", ""));
+        Course course = new Course("prueba", 100);
+        course.setStudents(students);
+        DataPersistence instance = DataPersistence.getInstance();    
+        instance.connectMongoDB();
+        boolean expResult = false;
+        boolean result = instance.updateStudentsInDB(course);
+        assertEquals(expResult, result);
+    }
+    
+        /**
+     * Test of updateStudentsInDB method, of class DataPersistence.
+     */
+    @Test
+    public void testUpdateStudentsCourseOneStudentWrong2() {
+        System.out.println("updateStudentsInDB");
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("", ""));
+        Course course = new Course("prueba", 100);
+        course.setStudents(students);
+        DataPersistence instance = DataPersistence.getInstance();    
+        instance.connectMongoDB();
+        boolean expResult = false;
+        boolean result = instance.updateStudentsInDB(course);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of updateStudentsInDB method, of class DataPersistence.
+     */
+    @Test
+    public void testUpdateStudentsCourseOneStudentWrong3() {
+        System.out.println("updateStudentsInDB");
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("L00243030", "Stephen"));
+        Course course = new Course("prueba", 100);
+        course.setStudents(students);
+        DataPersistence instance = DataPersistence.getInstance();    
+        instance.connectMongoDB();
+        boolean expResult = false;
+        boolean result = instance.updateStudentsInDB(course);
+        assertEquals(expResult, result);
+    }
+    
+        /**
+     * Test of updateStudentsInDB method, of class DataPersistence.
+     */
+    @Test
+    public void testUpdateStudentsCourseWrong() {
+        System.out.println("updateStudentsInDB");
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("L00243030", "Stephen"));
+        Course course = new Course("", 100);
+        course.setStudents(students);
+        DataPersistence instance = DataPersistence.getInstance();    
+        instance.connectMongoDB();
+        boolean expResult = false;
+        boolean result = instance.updateStudentsInDB(course);
+        assertEquals(expResult, result);
+    }
+    
+        /**
+     * Test of updateStudentsInDB method, of class DataPersistence.
+     */
+    @Test
+    public void testUpdateStudentsCourseWrong2() {
+        System.out.println("updateStudentsInDB");
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("L00243030", "Stephen"));
+        Course course = new Course("prueba", -10);
+        course.setStudents(students);
+        DataPersistence instance = DataPersistence.getInstance();    
+        instance.connectMongoDB();
+        boolean expResult = false;
         boolean result = instance.updateStudentsInDB(course);
         assertEquals(expResult, result);
     }
